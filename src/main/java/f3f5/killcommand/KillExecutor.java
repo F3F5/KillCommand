efd48c65f5a02d6df19408510e08c0cf588a92dd
MISHA <108226177+F3F5@users.noreply.github.com>
2024-02-15 10:23:36 +0400

    readable improvments

--- src/main/java/f3f5/killcommand/KillExecutor.java
+++ src/main/java/f3f5/killcommand/KillExecutor.java
@@ -1,5 +1,6 @@
 package f3f5.killcommand;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -7,16 +8,15 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 
-import static org.bukkit.Bukkit.getPlayer;
-
 public class KillExecutor implements CommandExecutor {
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!hasPermission(sender, "KillCommand.kill") || (args.length > 0 && !hasPermission(sender, "KillCommand.kill.others"))) {
             sender.sendMessage(ChatColor.RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
             return true;
         }
 
-        Player player = (args.length == 0 && sender instanceof Player) ? (Player) sender : (args.length > 0 ? getPlayer(args[0]) : null);
+        Player player = (args.length == 0 && sender instanceof Player) ? (Player) sender : (args.length > 0 ? Bukkit.getPlayer(args[0]) : null);
         if (player == null) {
             sender.sendMessage(ChatColor.RED + "Player not found.");
             return true;
